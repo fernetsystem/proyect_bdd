034260d7c3f3d668ab10323459151fa7011f271f
Fernet <fernetsystem@gmail.com>
2015-03-03 06:19:35 -0600

    Diseño1 de ventas
    Cambiar textbox por combobox y agregar iconos

--- Ventas.java
+++ Ventas.java
@@ -1,12 +1,17 @@
 import java.awt.event.*;
+import java.awt.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.sql.*;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 public class Ventas implements ActionListener {
 	JFrame f;
-	JLabel l1,l2,l3,l4,l5,l6,l7;
+	JLabel l1,l2,l3,l4,l5,l6,l7,bak;
 	JTextField text1,text2,text3,text4,text5,text6,text7;
+	Icon icono1,icono2,icono3,icono4,icono5,icono6;
 	JButton btn1,btn2,btn3;
+	JComboBox cmb1;
 	JTable table;
 	DefaultTableModel modelo;
 	JScrollPane src;
@@ -14,12 +19,13 @@ public class Ventas implements ActionListener {
     public Ventas() {
     f = new JFrame();
     l1 = new JLabel("No. Cuenta");
-    l2 = new JLabel("Fecha");
+    l2 = new JLabel(new ImageIcon("ic8.png"));
     l3 = new JLabel("Codigo");
     l4 = new JLabel("Cantidad");
     l5 = new JLabel("TOTAL");
     l6 = new JLabel("Recibo");
     l7 = new JLabel("Cambio");
+    bak = new JLabel(new ImageIcon("v5.jpg"));
     text1 =new JTextField();
     text2 =new JTextField();
     text3 =new JTextField();
@@ -27,9 +33,13 @@ public class Ventas implements ActionListener {
     text5 =new JTextField();
     text6 =new JTextField();
     text7 =new JTextField();
-    btn1 = new JButton("Añadir");
-    btn2 = new JButton("Quitar");
-    btn3 = new JButton("Realizarventa");
+    Icon icono1 = new ImageIcon(getClass().getResource("ic15.png")); //20,70,94
+    Icon icono2 = new ImageIcon(getClass().getResource("ic1.png"));  //66,100,114
+    Icon icono3 = new ImageIcon(getClass().getResource("ic33.png"));
+    btn1 = new JButton(icono1);	btn1.setBackground(new Color(111,135,143));		
+    btn2 = new JButton(icono2);	btn2.setBackground(new Color(111,135,143));		
+    btn3 = new JButton("Realizar venta",icono3); btn3.setBackground(new Color(111,135,143));		btn3.setForeground(Color.WHITE);	
+    cmb1 = new JComboBox();	cmb1.setBackground(new Color(255,255,255));//		cmb1.setForeground(Color.WHITE);
     modelo = new DefaultTableModel() {
 	   		@Override
 	   		public boolean isCellEditable(int fila, int columna) {
@@ -40,24 +50,26 @@ public class Ventas implements ActionListener {
 	src = new JScrollPane();
     }
     public void use(){
-    	f.setLayout(null);
-    	f.add(l1);	   	f.add(text1);
-    	f.add(l2);		f.add(text2);
-    	f.add(l3);		f.add(text3);
-    	f.add(l4);		f.add(text4);
-    	f.add(l5);		f.add(text5);
-    	f.add(l6);		f.add(text6);
-    	f.add(l7);		f.add(text7);
-    	f.add(src);
-    	f.add(btn1);
-    	f.add(btn2);
-    	f.add(btn3);
+    	f.add(bak);
+    	bak.setLayout(null);
+    	bak.add(l1);		bak.add(text1);
+    	bak.add(l2);		bak.add(text2);
+    	bak.add(l3);		bak.add(text3);
+    	bak.add(l4);		bak.add(text4);
+    	bak.add(l5);		bak.add(text5);
+    	bak.add(l6);		bak.add(text6);
+    	bak.add(l7);		bak.add(text7);
+    	bak.add(src);
+   		bak.add(btn1);    	
+   		bak.add(btn2);
+   		bak.add(btn3);
+   		bak.add(cmb1);
     	l1.setBounds	  (	18,	55,100,20);
     	text1.setBounds	  (	84,	55,100,20);
-    	l2.setBounds	  (384, 55, 37,20);
+    	l2.setBounds	  (376, 40, 50,50);
     	text2.setBounds	  (427, 55,100,20);
     	l3.setBounds	  ( 20,100, 40,20);
-    	text3.setBounds   ( 67,100,100,20);
+    	cmb1.setBounds    ( 67,100,100,20);
     	l4.setBounds	  (188,100,100,20);
     	text4.setBounds	  (243,100, 40,20);
     	src.setViewportView(table);
@@ -66,7 +78,7 @@ public class Ventas implements ActionListener {
 		modelo.addColumn("Nombre");
 		modelo.addColumn("Cantidad");
 		modelo.addColumn("Precio");
-		btn1.setBounds	 (330,135, 80,50);
+		btn1.setBounds	 (330,135, 80,50);	
 		btn2.setBounds	 (420,135, 80,50);
 		l5.setBounds	 (340,215,100,20);
     	text5.setBounds	 (390,215, 60,20);
@@ -74,14 +86,24 @@ public class Ventas implements ActionListener {
     	text6.setBounds	 (390,245, 60,20);
     	l7.setBounds	 (340,275,100,20);
     	text7.setBounds	 (390,275, 60,20);
- 		btn3.setBounds	 (330,330,130,50);
+ 		btn3.setBounds	 (330,330,160,50);
  		btn1.addActionListener(this);
  		btn2.addActionListener(this);
     	btn3.addActionListener(this);
-    
+		llena();    
     	f.setVisible(true);
     	f.setBounds		 (200,200,560,450);
     }
+    public void llena(){
+			try{ Class.forName("com.mysql.jdbc.Driver");
+			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/ekta?user=root&password=w9w9dorotea");
+			Statement instruccion = conexion.createStatement();
+			ResultSet tabla = instruccion.executeQuery("Select * from prendas_sucursal1;");
+			while(tabla.next()){ cmb1.addItem(tabla.getString(1));}
+			} catch(ClassNotFoundException e) { JOptionPane.showMessageDialog(null,e);}
+			catch(SQLException e) { System.out.println(e);JOptionPane.showMessageDialog(null,e);}
+		
+	}
     public void llenaTabla1(){
 			try{ Class.forName("com.mysql.jdbc.Driver");
 			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/ekta?user=root&password=w9w9dorotea");
@@ -103,15 +125,12 @@ public class Ventas implements ActionListener {
 		try{ Class.forName("com.mysql.jdbc.Driver");
 			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/ekta?user=root&password=w9w9dorotea");
 			Statement instruccion = conexion.createStatement();
-			ResultSet tabla = instruccion.executeQuery("Select * from prendas_sucursal1 where codigo="+text3.getText()+";");
+			ResultSet tabla = instruccion.executeQuery("Select * from prendas_sucursal1 where codigo="+cmb1.getSelectedItem()+";");
 			while(tabla.next()){ 
-                 existencia = tabla.getString(6);
-			     text5.setText(tabla.getString(6));
-			     
+                 existencia = tabla.getString(6);		     
 			}
 			} catch(ClassNotFoundException e) { JOptionPane.showMessageDialog(null,e);}
 			catch(SQLException e) { System.out.println(e);JOptionPane.showMessageDialog(null,e);}
-			JOptionPane.showMessageDialog(null,existencia);
 	}	
 	
     public void insertar_cuenta(){
@@ -136,15 +155,14 @@ public class Ventas implements ActionListener {
     	tot = exi-cant;
     	String resu = "";
     	resu = Integer.toString(tot);
-    	JOptionPane.showMessageDialog(null,tot);
-		PreparedStatement stmt = null,stmt2=null;
+    	PreparedStatement stmt = null,stmt2=null;
     	try{
     		Class.forName("com.mysql.jdbc.Driver");
     		Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/ekta?user=root&password=w9w9dorotea");
-    		String query1 = "INSERT INTO ventas_sucursal1 VALUES(NULL,"+text1.getText()+","+text3.getText()+","+text4.getText()+")";
+    		String query1 = "INSERT INTO ventas_sucursal1 VALUES(NULL,"+text1.getText()+","+cmb1.getSelectedItem() +","+text4.getText()+")";
     		stmt = conexion.prepareStatement(query1);
     		int retorno = stmt.executeUpdate();
-    		String query2 = "UPDATE prendas_sucursal1 SET existencia="+resu+" where codigo="+text3.getText();
+    		String query2 = "UPDATE prendas_sucursal1 SET existencia="+resu+" where codigo="+cmb1.getSelectedItem();
     		stmt2 = conexion.prepareStatement(query2);
     		int retorno2 = stmt2.executeUpdate();
     		if(retorno == 1){JOptionPane.showMessageDialog(null,"Se agrego una prenda");}
@@ -164,10 +182,10 @@ public class Ventas implements ActionListener {
     	try{
     		Class.forName("com.mysql.jdbc.Driver");
     		Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/ekta?user=root&password=w9w9dorotea");
-    		String query1 = "DELETE FROM ventas_sucursal1 WHERE codigo="+text3.getText()+" and idcuenta="+text1.getText();
+    		String query1 = "DELETE FROM ventas_sucursal1 WHERE codigo="+cmb1.getSelectedItem()+" and idcuenta="+text1.getText();
     		stmt = conexion.prepareStatement(query1);
     		int retorno = stmt.executeUpdate();
-    		String query2 = "UPDATE prendas_sucursal1 SET existencia="+resu+" where codigo="+text3.getText();
+    		String query2 = "UPDATE prendas_sucursal1 SET existencia="+resu+" where codigo="+cmb1.getSelectedItem();
     		stmt2 = conexion.prepareStatement(query2);
     		int retorno2 = stmt2.executeUpdate();
     		if(retorno == 1){JOptionPane.showMessageDialog(null,"Se removio una prenda");}
